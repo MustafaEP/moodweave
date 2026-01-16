1de1b9060760dfb547aab95ae8556a837b344c5f
MustafaEP <portakalm12@gmail.com>
2026-01-16 15:03:21 +0300

    chore(gateway): add rate limiting with NestJS throttler

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000, // 1 minute in milliseconds
+          limit: 60, // we allow 60 requests per minute
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
