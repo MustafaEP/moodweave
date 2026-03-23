9796d18e93d08a91a57c66d3a0ad4c821d80c09b
MustafaEP <portakalm12@gmail.com>
2026-03-23 13:58:18 +0300

    refactor(gateway): add ai and core's config urls

--- apps/gateway/src/config/app.config.ts
+++ apps/gateway/src/config/app.config.ts
@@ -3,4 +3,10 @@ export const appConfig = () => ({
     cors: {
         origin: process.env.CORS_ORIGIN || 'http://localhost:5137', 
     },
+    ai: {
+        url: process.env.AI_URL || 'http://moodweave-ai:8001',
+    },
+    core: {
+        url: process.env.CORE_URL || 'http://moodweave-core:8000',
+    }
 });
